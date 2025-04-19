564db3e81808000beb1bca3b945b19ec2d828f31
Freddy Zambrano <freddyfazq0614@gmail.com>
2025-04-19 18:56:03 -0500

    feat(product): :sparkles: Add to_json_api method for JSON serialization and update product retrieval

--- Apps/store/views/product/product.py
+++ Apps/store/views/product/product.py
@@ -62,7 +62,7 @@ class ProductDetailView(APIView):
     def get(self, request, product_id):
         try:
             product = Product.objects.get(id=product_id)
-            serializer = ProductSerializer(product, many=False, context={'request': request})
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            data = product.to_json_api(request=request)
+            return Response(data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
