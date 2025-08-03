4fc7ee5efe3c75acca930897c42a8296d0f8c021
FreddyAndres <fzambranoq2@unemi.edu.ec>
2025-08-03 09:28:37 -0500

    feat(cart-orders): :sparkles: add PageTitle component for improved title display in CartOrdersList and OrderHistoryList

--- frontend/src/modules/cart/pages/cartOrdersList.js
+++ frontend/src/modules/cart/pages/cartOrdersList.js
@@ -14,6 +14,8 @@ import DeleteModal from "../../../shared/components/molecules/DeleteModal";
 import {AnimatePresence, motion} from "framer-motion";
 import updateCartQuantity from "../utils/updateCartQuantity";
 import calculateTotals from "../utils/calculateTotals";
+import PageTitle from "../../../shared/components/atoms/PageTitle";
+import ROUTES from "../../../core/constants/routes/routes";
 
 const CartOrdersList = () => {
     const {carts: fetchedCarts, loading} = useGetCart();
@@ -52,18 +54,10 @@ const CartOrdersList = () => {
         <div className="bg-zinc-100 dark:bg-gray-900 flex-auto text-gray-900 dark:text-white flex flex-col">
             <Navbar/>
             <div className="flex items-center justify-between w-full px-4 py-2">
-                <GoBackButton redirectTo={"/dashboard"}/>
+                <GoBackButton redirectTo={ROUTES.DASHBOARD}/>
             </div>
 
-            <div className="container mx-auto px-6 sm:px-12 py-12">
-                <div className="flex flex-col sm:flex-row items-center sm:items-start gap-6">
-                    <div className="text-center sm:text-left">
-                        <h1 className="text-5xl sm:text-6xl font-bold bg-gradient-to-r from-orange-400 to-purple-500 bg-clip-text text-transparent">
-                            Lista de Pedidos
-                        </h1>
-                    </div>
-                </div>
-            </div>
+            <PageTitle title={"Lista de Pedidos"}/>
 
             {filteredCarts.length > 0 && (
                 <div className="flex justify-start mb-14 ml-20">
--- frontend/src/modules/orderHistory/pages/orderHistoryList.js
+++ frontend/src/modules/orderHistory/pages/orderHistoryList.js
@@ -1,6 +1,8 @@
 import Navbar from "../../../shared/components/organisms/Navbar";
 import GoBackButton from "../../../shared/components/molecules/GoBackButton";
 import React from "react";
+import PageTitle from "../../../shared/components/atoms/PageTitle";
+import ROUTES from "../../../core/constants/routes/routes";
 
 const OrderHistoryList = () => {
 
@@ -8,18 +10,10 @@ const OrderHistoryList = () => {
         <div className="bg-zinc-100 dark:bg-gray-900 flex-auto text-gray-900 dark:text-white flex flex-col">
             <Navbar/>
             <div className="flex items-center justify-between w-full px-4 py-2">
-                <GoBackButton redirectTo={"/dashboard"}/>
+                <GoBackButton redirectTo={ROUTES.DASHBOARD}/>
             </div>
 
-            <div className="container mx-auto px-6 sm:px-12 py-12">
-                <div className="flex flex-col sm:flex-row items-center sm:items-start gap-6">
-                    <div className="text-center sm:text-left">
-                        <h1 className="text-5xl sm:text-6xl font-bold bg-gradient-to-r from-orange-400 to-purple-500 bg-clip-text text-transparent">
-                            Historial de Pedidos
-                        </h1>
-                    </div>
-                </div>
-            </div>
+            <PageTitle title={"Historial de Pedidos"}/>
 
             <div className="container mx-auto px-6 sm:px-12 pb-12">
                 <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg">
