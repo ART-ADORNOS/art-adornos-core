3f11b3258c93b7465481f12fa64ada533a33a788
FreddyAndres <fzambranoq2@unemi.edu.ec>
2026-01-07 00:33:12 -0500

    refactor(api): :wrench: rename product serializers and views for clarity, implement product not found message, and enhance product handling logic

--- core/store/api/v1/product/serializers/product.py
+++ core/store/api/v1/product/serializers/product.py
@@ -3,10 +3,10 @@ from rest_framework import serializers
 from core.store.models import Product
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductInputSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'start_up', 'name', 'description', 'image', 'category', 'price', 'stock']
+        fields = '__all__'
 
     def to_representation(self, instance):
         return instance.to_json_api(request=self.context.get('request'))
