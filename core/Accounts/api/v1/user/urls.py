e132f6ce12dde2693ce01eac437574fe64e86c62
FreddyAndres <fzambranoq2@unemi.edu.ec>
2026-01-07 21:15:39 -0500

    refactor(api): :wrench: rename user views for consistency and clarity, and implement GetUserAPIView and RegisterUserAPIView

--- core/Accounts/api/v1/user/urls.py
+++ core/Accounts/api/v1/user/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from core.Accounts.api.v1.user import UserDeleteView, RegisterUserView, UpdateUserView, GetUserView
+from core.Accounts.api.v1.user import UserDeleteAPIView, RegisterUserAPIView, UpdateUserAPIView, GetUserAPIView
 
 urlpatterns = [
-    path('delete/', UserDeleteView.as_view(), name='delete_user'),
-    path('register/', RegisterUserView.as_view(), name='register'),
-    path('update/', UpdateUserView.as_view(), name='user_update'),
-    path('me/', GetUserView.as_view(), name='get_user'),
-
+    path('me/', GetUserAPIView.as_view(), name='get_user'),
+    path('register/', RegisterUserAPIView.as_view(), name='register'),
+    path('update/', UpdateUserAPIView.as_view(), name='user_update'),
+    path('delete/', UserDeleteAPIView.as_view(), name='delete_user')
 ]
