1b83db3fed6e7210194436f8b5a39839825d775c
FreddyAndres <fzambranoq2@unemi.edu.ec>
2026-01-18 14:05:09 -0500

    fix: :bug: update URL patterns to include trailing slashes for consistency

--- core/store/api/v1/startup/urls.py
+++ core/store/api/v1/startup/urls.py
@@ -4,9 +4,9 @@ from core.store.api.v1.startup import AllStartupsListAPIView, UserStartupsListAP
     StartupUpdateAPIView
 
 urlpatterns = [
-    path('all-startups', AllStartupsListAPIView.as_view(), name='all-startups'),
-    path('list', UserStartupsListAPIView.as_view(), name='user-startups-list'),
-    path('register', RegisterStartupAPIView.as_view(), name='register-startup'),
+    path('all-startups/', AllStartupsListAPIView.as_view(), name='all-startups'),
+    path('list/', UserStartupsListAPIView.as_view(), name='user-startups-list'),
+    path('register/', RegisterStartupAPIView.as_view(), name='register-startup'),
     path('update/<int:startup_id>', StartupUpdateAPIView.as_view(), name='update-startup'),
 
 ]
