b6529780ea00f009ba08d6ad412f47d0a9d28322
FreddyAndres <fzambranoq2@unemi.edu.ec>
2026-01-18 13:33:11 -0500

    feat: :sparkles: add JWT token authentication endpoint and fix user URL pattern

--- core/Accounts/api/v1/urls.py
+++ core/Accounts/api/v1/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
-    path('user', include('core.Accounts.api.v1.user.urls')),
+    path('user/', include('core.Accounts.api.v1.user.urls')),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
 ]
