4c0ee204d53eae3e649daab98ce26040acca8983
FreddyAndres <fzambranoq2@unemi.edu.ec>
2026-01-18 18:57:31 -0500

    fix: :bug: update URL patterns to include trailing slashes for consistency

--- core/store/api/v1/category/urls.py
+++ core/store/api/v1/category/urls.py
@@ -4,9 +4,9 @@ from core.store.api.v1.category import CategoryListAPIView, CategoryDeleteAPIVie
     CategoryUpdateAPIView
 
 urlpatterns = [
-    path('list/<int:startup_id>', CategoryListAPIView.as_view(), name='list_categories'),
-    path('register', RegisterCategoryAPIView.as_view(), name='register_category'),
-    path('update/<int:category_id>', CategoryUpdateAPIView.as_view(), name='update_category'),
-    path('delete/<int:category_id>', CategoryDeleteAPIView.as_view(), name='delete_category'),
+    path('list/<int:startup_id>/', CategoryListAPIView.as_view(), name='list_categories'),
+    path('register/', RegisterCategoryAPIView.as_view(), name='register_category'),
+    path('update/<int:category_id>/', CategoryUpdateAPIView.as_view(), name='update_category'),
+    path('delete/<int:category_id>/', CategoryDeleteAPIView.as_view(), name='delete_category'),
 
 ]
